2f6eae2c2ada3e42f0bf7dd72c9f4b9dbf6ecce0
Uros Aleksandrovic <uros.aleksandrovic@outlook.com>
2020-08-11 23:18:57 +0200

    Csv Data reader classes are added.

---
+++ src/SeoulAir.Device.Domain.Services/HelperClasses/CsvReader.cs
@@ -0,0 +1,67 @@
+﻿using SeoulAir.Device.Domain.Interfaces.HelperClasses;
+using System;
+using System.IO;
+
+namespace SeoulAir.Device.Domain.Services.HelperClasses
+{
+    public class CsvReader<TDto> : ICsvReader<TDto>
+        where TDto : class
+    {
+        private readonly string _dataPath;
+        private readonly IRowConverter<TDto> _rowConverter;
+        private StreamReader fileReader;
+
+        //TODO: implement configuration
+        //TODO: implement exceptions
+        //TODO: Inspect sonarLint message
+        public CsvReader(string dataPath, IRowConverter<TDto> rowConverter)
+        {
+            _dataPath = dataPath;
+            _rowConverter = rowConverter;
+        }
+
+        public void CloseFile()
+        {
+            if (fileReader == null)
+                return;
+
+            fileReader.Close();
+            fileReader.Dispose();
+            fileReader = null;
+        }
+
+        public void Dispose()
+        {
+            fileReader.Close();
+            fileReader.Dispose();
+        }
+
+        public void OpenFile()
+        {
+            if(fileReader != null)
+            {
+                fileReader.Close();
+                fileReader.Dispose();
+                fileReader = null;
+            }
+
+            if (!File.Exists(_dataPath))
+                throw new Exception();
+
+            fileReader = new StreamReader(_dataPath);
+        }
+
+        public bool TryReadNextRow(out TDto result)
+        {
+            if (fileReader.EndOfStream)
+            {
+                result = null;
+                return false;
+            }
+
+            string singleRow = fileReader.ReadLine();
+            result = _rowConverter.ConvertRow(singleRow.Split(','));
+            return true;
+        }
+    }
+}
---
+++ src/SeoulAir.Device.Domain/Dtos/StationInfoDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SeoulAir.Device.Domain.Dtos
+{
+    public class StationInfoDto
+    {
+        public ushort StationCode { get; set; }
+        public string StationAddress { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
